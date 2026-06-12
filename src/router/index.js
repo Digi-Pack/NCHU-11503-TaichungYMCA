95c73b6fbf3cd75f8154fe028322bec8b3ec714d
Brian-Chen0613 <xddxo14@gmail.com>
2026-06-12 16:34:38 +0800

    feat: 最新消息切版 (#15)

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import HomeView from "../views/HomeView.vue";
-import NewsView from "../views/NewsView.vue";
-import AboutView from "../views/AboutView.vue";
-import LocationsView from "../views/LocationsView.vue";
-import CoursesView from "../views/CoursesView.vue";
-import CourseDetailView from "../views/CourseDetailView.vue";
+import HomeView from "@/views/HomeView.vue";
+import NewsView from "@/views/NewsView.vue";
+import AboutView from "@/views/AboutView.vue";
+import LocationsView from "@/views/LocationsView.vue";
+import CoursesView from "@/views/CoursesView.vue";
+import CourseDetailView from "@/views/CourseDetailView.vue";
+import Test from "@/views/Test.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -41,6 +42,11 @@ const router = createRouter({
       name: "course-detail",
       component: CourseDetailView,
     },
+    {
+      path:"/test",
+      name:"test",
+      component:"Test"
+    }
   ],
 });
 
