c96d2948bdb2d4af0c6e7ba26bff9daf02458426
Brian-Chen0613 <xddxo14@gmail.com>
2026-06-12 13:34:52 +0800

    feat: 新增component (#12)

---
+++ src/components/PageHero.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+    image: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <svg width="1918" height="336" viewBox="0 0 1918 336" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <clipPath id="page-hero-clip">
+                <path d="M1522 336H0V0H1918V105C1918 279 1677.5 336 1522 336Z" />
+            </clipPath>
+        </defs>
+        <image :href="props.image" x="0" y="0" preserveAspectRatio="xMidYMid slice" clip-path="url(#page-hero-clip)" />
+    </svg>
+</template>
+
+<style scoped>
+svg {
+    display: block;
+    width: 100%;
+    height: auto;
+}
+
+image {
+    width: 100%;
+    height: 100%;
+}
+</style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,5 +1,15 @@
+<script setup>
+import PageHero from "@/components/PageHero.vue";
+import newsHeroImg from "@/assets/img/最新消息/最新消息測試圖.png";
+</script>
+
 <template>
-  <main>
-    <h1>最新消息</h1>
-  </main>
+  <section>
+   <PageHero :image="newsHeroImg"></PageHero>
+
+  </section>
 </template>
+
+<style scoped>
+
+</style>
