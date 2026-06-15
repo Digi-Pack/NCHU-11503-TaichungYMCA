1d0f8faab5c5381626dbd5db06a26e51cde6c7e4
Daisy6988 <umiminee@gmail.com>
2026-06-15 17:04:30 +0800

    feat:檔案內容修改 (#33)
    feat:檔案內容修改

---
+++ src/data/aboutData.js
@@ -0,0 +1,30 @@
+export const aboutCards = [
+  {
+    id: 1,
+    title: '終身學習',
+    summary: '提供多元課程與學習機會，培養自主學習力，成就更好的自己。',
+    date: '2026-01-01',
+    image: 'https://picsum.photos/seed/n1/765/806',
+  },
+  {
+    id: 2,
+    title: '公民參與',
+    summary: '培養公民素養與行動力，參與公共事務，打造友善共好的社會。',
+    date: '2026-01-02',
+    image: 'https://picsum.photos/seed/n2/765/806',
+  },
+  {
+    id: 3,
+    title: '關懷在地',
+    summary: '關心在地議題與需求，實踐社會關懷，共創永續家園。',
+    date: '2026-01-03',
+    image: 'https://picsum.photos/seed/n3/765/806',
+  },
+  {
+    id: 4,
+    title: '永續行動',
+    summary: '推廣環境永續與社會共榮，從在地出發，實踐永續未來。',
+    date: '2026-01-04',
+    image: 'https://picsum.photos/seed/n4/765/806',
+  },
+]
