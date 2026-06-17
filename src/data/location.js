8ea138b0ad44dcf45ac201b4005a5d51b8e61c89
Daisy6988 <umiminee@gmail.com>
2026-06-17 11:42:59 +0800

    feat:新增location.js (#48)
    feat:新增location.js

--- src/data/location.js
+++ src/data/location.js
@@ -1,70 +1,107 @@
 // src/data/locations.js
-import img1 from '../assets/img/服務據點/location-1.jpg'
-import img2 from '../assets/img/服務據點/location-2.jpg'
-import img3 from '../assets/img/服務據點/location-3.jpg'
-import img4 from '../assets/img/服務據點/location-4.jpg'
-import img5 from '../assets/img/服務據點/location-5.jpg'
-import img6 from '../assets/img/服務據點/location-6.jpg'
+import img1 from '@/assets/img/服務據點/location-1.jpg'
+import img2 from '@/assets/img/服務據點/location-2.jpg'
+import img3 from '@/assets/img/服務據點/location-3.jpg'
+import img4 from '@/assets/img/服務據點/location-4.jpg'
+import img5 from '@/assets/img/服務據點/location-5.jpg'
+import img6 from '@/assets/img/服務據點/location-6.jpg'
+import img7 from '@/assets/img/服務據點/location-7.jpg'
+import img8 from '@/assets/img/服務據點/location-8.jpg'
 
 export const allLocations = [
-  {
+{
     id: 1,
-    name: '北屯會館',
-    alias: '北屯本部',
-    description: '404臺中市北區錦祥里北屯路10號5樓',
-    tel: '0422328823',
+    name: "北屯分部",
+    alias: "北屯會館",
+    description: "臺中市北區錦祥里北屯路10號5樓",
+    tel: "04-2232-8823",
     image: img1,
-    region: '北屯本部',
-    date: '2026-06-16'
+    region: "北屯分部",
+    date: "2026-06-16",
+    mapUrl:"https://www.google.com/maps/search/?api=1&query=臺中市北區錦祥里北屯路10號5樓"
   },
+
   {
     id: 2,
-    name: '松竹分部',
-    alias: '松竹國小',
-    description: '406臺中市北屯區松和里昌平路二段12號',
-    tel: '0422328823',
+    name: "松竹分部",
+    alias: "松竹國小",
+    description: "臺中市北屯區松和里昌平路二段12號",
+    tel: "04-2232-8823",
     image: img2,
-    region: '松竹分部',
-    date: '2026-06-16'
+    region: "松竹分部",
+    date: "2026-06-16",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=臺中市北屯區松和里昌平路二段12號"
   },
+
   {
     id: 3,
-    name: '東山分部',
-    alias: '東山高中',
-    description: '406臺中市北屯區水景里景賢六路200號',
-    tel: '0422328823',
+    name: "東山分部",
+    alias: "東山高中",
+    description: "臺中市北屯區水景里景賢六路200號",
+    tel: "04-2232-8823",
     image: img3,
-    region: '東山分部',
-    date: '2026-06-16'
+    region: "東山分部",
+    date: "2026-06-16",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=臺中市北屯區水景里景賢六路200號"
   },
+
   {
     id: 4,
-    name: '大德分部',
-    alias: '大德國中',
-    description: '406臺中市北屯區陳平里雷中街2-23號',
-    tel: '0422328823',
+    name: "大德分部",
+    alias: "大德國中",
+    description: "臺中市北屯區陳平里雷中街2-23號",
+    tel: "04-2232-8823",
     image: img4,
-    region: '大德分部',
-    date: '2026-06-16'
+    region: "大德分部",
+    date: "2026-06-16",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=臺中市北屯區陳平里雷中街2-23號"
   },
+
   {
     id: 5,
-    name: '陳平分部',
-    alias: '陳平國小',
-    description: ' 406臺中市北屯區忠平里陳平路58號',
-    tel: '0422328823',
+    name: "陳平分部",
+    alias: "陳平國小",
+    description: "臺中市北屯區忠平里陳平路58號",
+    tel: "04-2232-8823",
     image: img5,
-    region: '陳平分部',
-    date: '2026-06-16'
+    region: "陳平分部",
+    date: "2026-06-16",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=臺中市北屯區忠平里陳平路58號"
   },
+
   {
     id: 6,
-    name: '仁美分部',
-    alias: '仁美國小',
-    description: '406臺中市北屯區仁美里崇德路三段568號',
-    tel: '0422328823',
+    name: "仁美分部",
+    alias: "仁美國小",
+    description: "臺中市北屯區仁美里崇德路三段568號",
+    tel: "04-2232-8823",
     image: img6,
-    region: '仁美分部',
-    date: '2026-06-16'
-  }
-];
+    region: "仁美分部",
+    date: "2026-06-16",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=臺中市北屯區仁美里崇德路三段568號"
+  },
+
+  {
+    id: 7,
+    name: "文昌分部",
+    alias: "文昌國小",
+    description: "臺中市北屯區平昌里昌平路一段91號",
+    tel: "04-2232-8823",
+    image: img7,
+    region: "文昌分部",
+    date: "2026-06-16",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=臺中市北屯區平昌里昌平路一段91號"
+  },
+
+  {
+    id: 8,
+    name: "文昌分部",
+    alias: "東光活動中心",
+    description: "臺中市北屯區東光里東山路一段33巷16號",
+    tel: "04-2232-8823",
+    image: img8,
+    region: "文昌分部",
+    date: "2026-06-16",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=臺中市北屯區東光里東山路一段33巷16號"
+  },
+  ];
